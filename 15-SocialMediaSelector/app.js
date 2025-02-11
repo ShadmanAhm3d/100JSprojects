036d92cd7e992368738501be2d0a42e083bd8084
ShadmanAhm3d <shadman01010@gmail.com>
2025-02-11 22:36:28 +0530

    (^_^)

---
+++ 15-SocialMediaSelector/app.js
@@ -0,0 +1,8 @@
+const first = document.getElementById("first");
+
+const value = document.querySelectorAll(".value");
+
+
+console.log(value); //why is this not returning a all the elements
+console.log(first);
+
