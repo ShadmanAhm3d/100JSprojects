00c7d8024dfb83061fba89c144dfc2dbf9e8e5ac
ShadmanAhm3d <shadman01010@gmail.com>
2025-02-16 13:09:34 +0530

    (^_^)

---
+++ 21-RandomEmojiGenerator/app.js
@@ -0,0 +1,30 @@
+const btn = document.getElementById("id-btn");
+
+
+
+
+let response = [];
+
+document.addEventListener("DOMContentLoaded", async ()=>{
+   response = await fetch("https://emoji-api.com/emojis?access_key=e8e3b9a983facb9e9e361dbf44c04294f0e2365a").then((data)=>data.json());
+})
+
+async function updateDOM(){
+
+  const pTag = document.querySelector('#id-btn p');
+  const randomNumber = Math.floor(Math.random() * 1857);
+  pTag.textContent = `${response[randomNumber].character}`;
+  console.log(response[randomNumber])
+  
+
+  const pTagDesc = document.querySelector('#content');
+  console.log(pTagDesc);
+  pTagDesc.innerText = `${response[randomNumber].unicodeName}`;
+}
+
+
+
+btn.addEventListener("click",()=>{
+  console.log("Clikced")
+  updateDOM();
+})
